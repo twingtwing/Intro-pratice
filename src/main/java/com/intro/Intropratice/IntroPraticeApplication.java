2717f413796f4a119a111ce2918fe28648bb14d7
twingtwing <soyoungdev.7@gmail.com>
2022-09-07 23:40:59 +0900

    First Commit

---
+++ src/main/java/com/intro/Intropratice/IntroPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.intro.Intropratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroPraticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intro/Intropratice/IntroPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intro.Intropratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
